a26c2a04c333aa3a4fb022fdffddb9b69ef876ec
Arjun Khunti <arjun.khunti@CDSYS.LOCAL>
2019-12-30 21:29:19 +0530

    props, state and setState implemented

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message';
+import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
+      <Counter />
+      {/* <Message /> */}
       {/* This is use of props in function components */}
-      <Greet name="Arjun" heroName="Iron Man">
+      {/* <Greet name="Arjun" heroName="Iron Man">
         <p>This is a children tag!</p>
       </Greet>
       <Greet name="Ritesh" heroName="Hulk">
         <button>Action</button>
       </Greet>
-      <Greet name="Yug" heroName="Captain America"/>
+      <Greet name="Yug" heroName="Captain America"/> */}
 
       {/* This is use of props in Class components */}
-      <Welcome name="Arjun" heroName="Iron Man"/>
+      {/* <Welcome name="Arjun" heroName="Iron Man"/>
       <Welcome name="Ritesh" heroName="Hulk"/>
-      <Welcome name="Yug" heroName="Captain America"/>
+      <Welcome name="Yug" heroName="Captain America"/> */}
 
 
       {/* <Hello />  */}
---
+++ src/components/Counter.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment() {
+    // this.setState(
+    //   {
+    //     count: this.state.count + 1
+    //   },
+    //   () => {
+    //     console.log("Callback Value", this.state.count);
+    //   }
+    // );
+    //  one less than original value
+    console.log(this.state.count);
+
+    this.setState((prevState, props) => ({
+        count: prevState.count + 1 //props.addValue
+    }))
+  }
+
+  incrementFive() {
+    this.increment();
+    this.increment();
+    this.increment();
+    this.increment();
+    this.increment();
+  }
+
+  render() {
+    return (
+      <div>
+        <div>count - {this.state.count}</div>
+        <button onClick={() => this.incrementFive()}>Increment </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
